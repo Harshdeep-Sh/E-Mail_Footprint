fcff9eed7fe4b0d500fcd32c65395fc9bb9a245d
Harshdeep Shrivastava <harshri2015@gmail.com>
2023-09-03 22:25:11 +0530

    Logic update

--- app.py
+++ app.py
@@ -1,47 +1,10 @@
-# contains the logic behind the output
+
+import re
+
 
 def urlchkk(n):
+    ipv4_pattern = r'\b(?:\d{1,3}\.){3}\d{1,3}\b'
+    ipv4_addresses = re.findall(ipv4_pattern, text)
+    for address in ipv4_addresses:
+        return (address)
     
-    return 
-#     if n.count("https://")==1:
-#         if len(n)<75:
-#             if (n.find("$")==-1 or n.find("@")==-1):
-#                 if (n.find("tiny")==-1 or n.find("bit.ly")==-1):
-#                     name=""
-#                     if n.find("www")==-1:
-#                         dotpos=n.find(".")
-#                         name=n[8:dotpos]
-#                         if n.count(name)==1:
-#                             return"This URL might be safe. You may go ahead."
-#                         else:
-#                             return"Proceed with Caution. Click only if you TRUST the Sender."
-#                             return"no www double"
-#                     elif n.find("www")!=-1:
-#                         dotpos=0
-#                         urll=n
-#                         for i in range(2):
-#                             dotpos=urll.find(".")
-#                             urll=urll[dotpos+1:]
-#                         dotpos=11+dotpos+1
-#                         name=n[12:dotpos]
-#                         if n.count(name)==1:
-#                             return"This URL might be safe. You may go ahead."
-#                         else:
-#                             return"Proceed with Caution. Click only if you TRUST the Sender."
-#                             return"with www double"
-#                 else:
-#                     "Proceed with Caution. Click only if you TRUST the Sender."
-#                     #return"tiny bitly")
-#             else:
-#                 return"Proceed with Caution. Click only if you TRUST the Sender."
-#                 #return"special sym")
-#         else:
-#             return"Proceed with Caution. Click only if you TRUST the Sender."
-#             #return"len")
-#     else:
-#         return"Proceed with Caution. Click only if you TRUST the Sender."
-#         #return"count")
-    
-#     if (n.find("tiny")!=-1 or n.find("bit.ly")!=-1):
-#         return"This might me unsafe. Click only if You trust the sender."
-
